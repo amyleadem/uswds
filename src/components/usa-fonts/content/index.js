84c7141aa2ad9ca77988af6471cd50e68890da1c
Amy Leadem <amy.leadem@bixal.com>
2021-12-08 16:05:58 -0700

    export scss variables for fonts component story

--- src/components/usa-fonts/content/index.js
+++
@@ -1,2 +0,0 @@
-export { default as StyleSans } from "./usa-fonts~sans.json";
-export { default as StyleSerif } from "./usa-fonts~serif.json";
--- src/components/usa-fonts/fonts.stories.js
+++ src/components/usa-fonts/fonts.stories.js
@@ -1,8 +1,8 @@
 import Component from "./usa-fonts.twig";
-import {
-  StyleSans,
-  StyleSerif
-} from "./content";
+import fontStyles from "../../stylesheets/theme/_export.module.scss";
+
+const TypeSerif = fontStyles.typeSerif.replace(/["']/g, "");
+const TypeSans = fontStyles.typeSans.replace(/["']/g, "");
 
 export default {
   title: "Tokens/Fonts",
@@ -19,6 +19,7 @@ export default {
         'Verdana',
       ],
       control: { type: 'select' },
+      defaultValue: TypeSans,
       table: { disable: true },
     },
     id: {
@@ -35,7 +36,7 @@ export default {
         'italic',
       ],
       control: { type: 'select' },
-      defaultValue: 'roman',
+      defaultValue: 'normal',
       table: { disable: true }
     },
     font_weight: {
@@ -54,16 +55,36 @@ export default {
       defaultValue: '400',
       table: { disable: true }
     },
+    description: {
+      control: { type: 'text' },
+      defaultValue: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt varius purus eget feugiat. Pellentesque ultricies mauris eget leo gravida, a viverra ante facilisis. Donec ac varius justo. Sed malesuada faucibus dui, eget euismod ante dictum sit amet. Integer quis congue lacus. Proin et egestas nibh. Nulla efficitur molestie bibendum. Aenean at diam sit amet risus facilisis molestie quis eget metus. Morbi sit amet bibendum lacus. Quisque congue pulvinar tempor. Ut molestie mi nec massa ullamcorper semper.'
+    }
   },
 } 
 
 const Template = (args) => Component(args);
 
 export const SansSerif = Template.bind({});
-SansSerif.args = StyleSans;
+SansSerif.argTypes = {
+  font_token: {
+    defaultValue: TypeSans,
+  },
+  id: {
+    defaultValue: "sans",
+    table: { disable: false },
+  },
+}
 
 export const Serif = Template.bind({});
-Serif.args = StyleSerif;
+Serif.argTypes = {
+  font_token: {
+    defaultValue: TypeSerif,
+  },
+  id: {
+    defaultValue: "serif",
+    table: { disable: false },
+  },
+}
 
 export const StylePlayground = Template.bind({});
 StylePlayground.argTypes = {
@@ -72,7 +93,6 @@ StylePlayground.argTypes = {
   },
   font_token: {
     table: { disable: false },
-    defaultValue: 'Source Sans Pro Web',
   },
   font_style: {
     table: { disable: false }
@@ -80,8 +100,4 @@ StylePlayground.argTypes = {
   font_weight: {
     table: { disable: false }
   },
-  description: {
-    control: { type: 'text' },
-    defaultValue: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt varius purus eget feugiat. Pellentesque ultricies mauris eget leo gravida, a viverra ante facilisis. Donec ac varius justo. Sed malesuada faucibus dui, eget euismod ante dictum sit amet. Integer quis congue lacus. Proin et egestas nibh. Nulla efficitur molestie bibendum. Aenean at diam sit amet risus facilisis molestie quis eget metus. Morbi sit amet bibendum lacus. Quisque congue pulvinar tempor. Ut molestie mi nec massa ullamcorper semper.'
-  }
 }
