dc3d49554c89b8596da7f680c6cb7ebe57c7ecc8
Amy Leadem <amy.leadem@bixal.com>
2021-12-09 14:29:33 -0700

    add tag component story

---
+++ src/components/usa-tag/content/index.js
@@ -0,0 +1,2 @@
+export { default as StyleDefault } from "./usa-tag.json";
+export { default as StyleBig } from "./usa-tag~big.json";
---
+++ src/components/usa-tag/tag.stories.js
@@ -0,0 +1,27 @@
+import Component from "./usa-tag.twig";
+import {
+  StyleDefault,
+  StyleBig,
+} from "./content";
+
+
+export default {
+  title: "Tokens/Tag",
+  argTypes: {
+    modifier: {
+      control: { type: 'select' },
+      options: [
+        'usa-tag',
+        'usa-tag usa-tag--big',
+      ],
+    }
+  }
+}
+
+const Template = (args) => Component(args);
+
+export const Default = Template.bind({});
+Default.args = StyleDefault;
+
+export const Big = Template.bind({});
+Big.args = StyleBig;
