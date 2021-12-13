99626c1ec52d13a08c308565167007dc8d23802f
Amy Leadem <amy.leadem@bixal.com>
2021-12-13 15:25:23 -0700

    created color items in object

--- src/tokens/usa-colors/usa-colors.stories copy.js
+++ src/tokens/usa-colors/usa-colors.stories copy.js
@@ -1,34 +1,26 @@
 import Component from "./usa-colors.twig";
 import * as data from './test.json';
-import {
-  Blue,
-  Red, 
-  Yellow
-} from "../../data/colors";
 const arr = [];
 const arrayValues = arr.values();
-const arrows = { Red, Blue, Yellow };
-const myObject = {};
  
 for (let i = 0; i < 10; i++) {
   const colorFamily = data.props[0];
   const colorValues = data.props[0].value[i];
-  const colorName = colorFamily.name + "-" + colorValues.name;
-  const colorHex = colorValues.value;
-  myObject[colorName] = colorHex;
+  const value = "'" + colorFamily.name + "-" + colorValues.name + "= " + colorValues.value;
+  arr.push(value);
 };
 
 for (let elements of arrayValues) {
   console.log(elements);
 };
 
-console.log(myObject);
-
 export default {
   title: "Tokens/Colors",
   argTypes: {
     modifier: {
-      options: Object.keys(myObject),
+      options: [
+        arrayValues,
+      ],
       control: { type: 'select' },
     },
     display_element_states: {
