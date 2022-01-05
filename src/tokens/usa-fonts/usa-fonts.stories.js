8a7a737ce05c59c5d96c24e9af16ea456e0720ed
Amy Leadem <amy.leadem@bixal.com>
2022-01-05 15:55:53 -0700

    add fonts stories

---
+++ src/tokens/usa-fonts/usa-fonts.stories.js
@@ -0,0 +1,141 @@
+import Component from "./usa-fonts.twig";
+import * as vars from "./usa-fonts~vars";
+console.log(vars.themeFontSerif);
+console.log(vars.systemTypefaces);
+
+export default {
+  title: "Tokens/Fonts",
+  argTypes: {
+    font_family_headline: {
+      name: 'Headline - font family token',
+      control: { type: 'select' },
+      defaultValue: 'Merriweather',
+      options: vars.systemTypefaces,
+      table: { disable: true },
+    },
+    font_style_headline: {
+      control: { type: 'select' },
+      defaultValue: 'normal',
+      name: 'Headline - font style',
+      options: vars.systemFontStyle,
+      table: { disable: true },
+    },
+    font_weight_headline: {
+      control: { type: 'select' },
+      defaultValue: '600',
+      name: 'Headline - font weight',
+      options: vars.systemFontWeight,
+      table: { disable: true }
+    },
+    font_family_body: {
+      control: { type: 'select' },
+      defaultValue: 'Source Sans Pro',
+      mapping: vars.systemTypefaces,
+      name: 'Body - font family token',
+      options: vars.systemTypefaces,
+      table: { disable: true },
+    },
+    font_style_body: {
+      control: { type: 'select' },
+      defaultValue: 'normal',
+      name: 'Body - font style',
+      options: vars.systemFontStyle,
+      table: { disable: true },
+    },
+    font_weight_body: {
+      control: { type: 'select' },
+      defaultValue: '400',
+      name: 'Body - font weight',
+      options: vars.systemFontWeight,
+      table: { disable: true }
+    },
+    text_width: {
+      control: { type: 'range', min: 1, max: 6, step: 1 },
+      defaultValue: 3,
+      name: 'Text width increments',
+      table: { disable: true },
+    },
+    id: {
+      defaultValue: "sans",
+      name: 'Theme font family',
+      table: { disable: true },
+    },
+    playground_mode: {
+      control: { type: 'boolean' },
+      defaultValue: 'false',
+      table: { disable: true },
+    },
+  },
+} 
+
+const Template = (args) => Component(args);
+
+export const ThemeMono = Template.bind({});
+ThemeMono.argTypes = {
+  font_family_headline: {
+    defaultValue: vars.themeFontMono,
+  },
+  font_family_body: {
+    defaultValue: vars.themeFontMono,
+  },
+  id: {
+    defaultValue: "mono",
+  },
+}
+
+export const ThemeSansSerif = Template.bind({});
+ThemeSansSerif.argTypes = {
+  font_family_headline: {
+    defaultValue: vars.themeFontSans,
+  },
+  font_family_body: {
+    defaultValue: vars.themeFontSans,
+  },
+  id: {
+    defaultValue: "sans",
+  },
+}
+
+export const ThemeSerif = Template.bind({});
+ThemeSerif.argTypes = {
+  font_family_headline: {
+    defaultValue: vars.themeFontSerif,
+  },
+  font_family_body: {
+    defaultValue: vars.themeFontSerif,
+  },
+  id: {
+    defaultValue: "serif",
+  },
+}
+
+export const StylePlayground = Template.bind({});
+StylePlayground.argTypes = {
+  playground_mode: {
+    defaultValue: 'true',
+  },
+  font_family_headline: {
+    table: { disable: false },
+  },
+  font_style_headline: {
+    table: { disable: false }
+  },
+  font_weight_headline: {
+    table: { disable: false }
+  },
+  typescale_h1: {
+    table: { disable: false }
+  },
+  font_family_body: {
+    table: { disable: false },
+  },
+  font_style_body: {
+    table: { disable: false },
+  },
+  font_weight_body: {
+    table: { disable: false },
+  },
+  text_width: {
+    table: { disable: false },
+  },
+}
---
+++ src/tokens/usa-fonts/usa-fonts~vars.js
@@ -0,0 +1,62 @@
+import system from "../../data/core-vars.json";
+import theme from "../../data/theme-vars.json";
+
+// System font vars
+export const systemTypefaceList= system.systemTypefaceTokens[0];
+export const systemTypefaces = [];
+
+for (let t = 0; t <  systemTypefaceList.mapValue.length; t += 1) {
+    const typefaceMap =  systemTypefaceList.mapValue[t];
+    const typefaceName = typefaceMap.name;
+    systemTypefaces.push(typefaceName);
+}
+
+// Theme font vars
+const themeFonts = theme.themeFontType;
+export const themeFontsList = {}; 
+
+for (let tf = 0; tf < themeFonts.length; tf += 1) {
+    const item = themeFonts[tf];
+    const itemValue = item.value;
+    const itemName = item.name;
+  
+    if (itemName.includes("mono")) {  
+        const mono = "Mono";
+        themeFontsList[mono] = itemValue.replaceAll("-"," ");  
+    } else if (itemName.includes("sans")) {  
+        const sans = "Sans";
+        themeFontsList[sans] = itemValue.replaceAll("-"," ");  
+    }
+    else if (itemName.includes("serif")) {  
+        const serif = "Serif";
+        const serifValue = itemValue.replaceAll("-"," ");
+        themeFontsList[serif] = serifValue ;  
+    }
+}
+
+export const themeFontMono = themeFontsList.Mono;
+export const themeFontSans = themeFontsList.Sans;
+export const themeFontSerif = themeFontsList.Serif;
+
+
+export const systemFontStyle = [
+    'normal',
+    'italic',
+]
+
+export const systemFontWeight = [
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+]
+
+
+// const lineHeightList= vars.systemLineHeight[0];
+// export const lineHeightListParsed = JSON.parse(lineHeightList);
+// export const systemLineHeight = {};*/
